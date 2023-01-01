0b4674cfeb268252f977a0e39d58d77834dd2d2c
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-01 15:26:59 -0800

    9 task

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last = number % 10
+    l = number % 10
     if number < 0:
-        last = last - 10
-    return last
+        l = l - 10
+    return l
