ac80ca995268211fb0fbfc2cd47a884317fc3fe0
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-01 16:09:11 -0800

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for k in range(101):
+    for k in range(1, 101):
         if k % 3 == 0 and k % 5 == 0:
             print("FizzBuzz", end=" ")
         elif k % 3 == 0:
