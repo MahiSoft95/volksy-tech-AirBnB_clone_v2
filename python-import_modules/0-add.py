df787999141afa7d7ac5b0031580eece6bb5e5ef
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-03 17:55:19 -0800

    helloo

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-rs = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, rs))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
