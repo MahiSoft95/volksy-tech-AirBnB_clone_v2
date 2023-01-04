87695506fff02a880dcd5d6ed159cdaa5b1ef56e
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-04 04:38:21 -0800

    calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
     b = int(argvlist[2])
     opr = argvlist[1]
     if opr == '+':
-        res=add(a,b)
+        res = add(a, b)
     elif opr == '-':
-        res=sub(a,b)
+        res = sub(a, b)
     elif opr == '*':
-        res=mul(a,b)
+        res = mul(a, b)
     elif opr == '/':
-        res=div(a,b)
+        res = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print("{}+{}={}".div(a, b, res))
+    print("{}+{}={}".format(a, b, res))
     exit()
