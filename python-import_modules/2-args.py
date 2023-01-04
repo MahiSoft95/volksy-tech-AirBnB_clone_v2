659e42b1f1abec5f95e4815c58cd5fdad66627ce
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-03 20:10:29 -0800

    message

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,12 @@ if __name__ == "__main__":
     from sys import argv
     x = argv[1:]
     if len(x) == 0:
-        print("{} arguments.".format(len(x)))
+        print("{} arguments:".format(len(x)))
+    elif len(x) == 1:
+        print("{} argument:".format(len(x)))
     else:
-        print("{} arguments.".format(len(x)))
-        n = 1
-        for k in x:
-            print("{}: {}".format(n, k))
-            n = n + 1
+        print("{} arguments:".format(len(x)))
+    n = 1
+    for k in x:
+        print("{}: {}".format(n, k))
+        n = n + 1
