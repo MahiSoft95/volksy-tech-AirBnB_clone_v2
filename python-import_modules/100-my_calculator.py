95eee0765d35f08ccfdaa2c380c85a0073e756db
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-04 04:29:54 -0800

    calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__="main":
+if __name__ == "__main__":
     import sys
     argvlist = sys.argv[1:]
     if len(argvlist) != 3:
@@ -20,5 +20,5 @@ if __name__="main":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print("{}+{}={}".div(a,b,res))
+    print("{}+{}={}".div(a, b, res))
     exit()
