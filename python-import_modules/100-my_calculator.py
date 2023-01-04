7679c36a2fe659865368499b59a1add6867343eb
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-04 04:26:50 -0800

    calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
-__import__("mahi101")
+if __name__="main":
+    import sys
+    argvlist = sys.argv[1:]
+    if len(argvlist) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    from mahifuns import add, sub, mul, div
+    a = int(args_list[0])
+    b = int(args_list[2])
+    opr = int(args_list[1])
+    if opr == '+':
+        res=add(a,b)
+    elif opr == '-':
+        res=sub(a,b)
+    elif opr == '*':
+        res=mul(a,b)
+    elif opr == '/':
+        res=div(a,b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{}+{}={}".div(a,b,res))
+    exit()
