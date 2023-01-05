b8aa42d772fd3a2b19ceb474df335781700125f0
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-05 03:09:28 -0800

    hellooo

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,13 +4,5 @@ def new_in_list(my_list, idx, element):
         return mylist
     else:
         my_list[idx] = element
+        new_list = my_list
         return my_list
-
-    
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
