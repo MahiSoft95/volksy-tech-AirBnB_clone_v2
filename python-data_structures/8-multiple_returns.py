c54680ba99af34d41bd42c999a940fcac4a072ea
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-06 03:06:39 -0800

    matrixxxx

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     a = len(sentence)
-    b = sentence[0]
+    if a !=0:
+        b = sentence[0]
+    else:
+        b = None
     return (a, b)
