f2aa72f5b61ef54021d4dc84363c36deb94c0570
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-06 02:52:21 -0800

    matrixxxx

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,5 +5,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b = tuple_b + tuple_c
     element1 = tuple_a[0] + tuple_b[0]
     element2 = tuple_a[1] + tuple_b[1]
-    newtuple =(element1, element2)
+    newtuple = (element1, element2)
     return newtuple
