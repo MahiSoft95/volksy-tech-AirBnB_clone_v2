b46ea81e0c9b69cdae1fbb0a7607431e2333aa62
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-06 03:17:38 -0800

    matrixxxx

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        newlist=[]
-        newlst[0]=0
+        newlist = []
+        newlist[0] = 0
         for i in my_list:
             if i > newlist[0]:
                 newlist[0] = i
