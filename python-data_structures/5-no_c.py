118ace3509e2a2c7ca510e24425259606dedfd8b
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-05 23:53:24 -0800

    hellloo

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for k in range(len(my_string)):
         if my_string[k] != 'c' or my_string[k] != 'C':
             new_str += my_string
-    return new_string
+    return new_str
