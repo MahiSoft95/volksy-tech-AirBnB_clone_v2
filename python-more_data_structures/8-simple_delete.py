9fd596eabad5d5c8a9a674a20cb3141c524df63e
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-08 10:54:26 -0800

    helloo set dict

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
-    return a_dictionary
+    for k in a_dictionary:
+        if k == key:
+            del a_dictionary[key]
+    return a_dictionariy
