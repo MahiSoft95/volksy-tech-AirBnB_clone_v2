84242537411b1a13a06716d9a0ac28f4367a6c4f
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-08 14:11:46 -0800

    helloo set dict

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,11 +4,11 @@ def roman_to_int(s):
         return 0
     if type(s) != str:
         return 0
-    translations = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
+    trans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     s = s.replace("IV", "IIII").replace("IX", "VIIII")
     s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
     s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
     number = 0
     for char in s:
-        number += translations[char]
+        number += trans[char]
     return number
