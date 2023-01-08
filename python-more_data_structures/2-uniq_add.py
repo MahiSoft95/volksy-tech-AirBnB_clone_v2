7da5b835bf57fadd815873565d8f1da2215fc056
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-08 10:02:22 -0800

    helloo set dict

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     total = 0
     newlist = sorted(my_list[:])
-    for i in newlist:
+    for i in range(len(newlist)):
         if i == 0 or newlist[i] != newlist[i-1]:
             total += newlist[i]
     return total
