4e72af7f3cb87b05c2bcfffe31de579c3723acf3
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-08 11:11:42 -0800

    helloo set dict

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    newdict = a_dictionary
+    newdiict = {}
+    newdict += a_dictionary
     for k, v in newdict.items():
         newdict[k] = v*2
     return newdict
