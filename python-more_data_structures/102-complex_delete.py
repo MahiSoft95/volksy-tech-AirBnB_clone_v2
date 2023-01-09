5e7e5ff6a61dd435b643eb8abad03fd74c1adf46
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-08 16:03:43 -0800

    helloo set dict

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    temp = False
+    if a_dictionary:
+        for k, v in a_dictionary.items():
+            if v == value:
+                temp = k
+        if temp:
+            del a_dictionary[k]
+            return(complex_delete(a_dictionary, value)
+    return a_dictionary)
