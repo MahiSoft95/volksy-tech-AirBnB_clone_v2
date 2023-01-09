5443393cc31e4a3ee45fffa88c67b3dfda64a5b6
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-08 18:34:50 -0800

    python exceptions

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     newlist = [0] * list_length
     for i in range(list_length):
         try:
-            quotient = my_list1[i] / my_list2[i]
+            quotient = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
             print("division by 0")
         except TypeError:
