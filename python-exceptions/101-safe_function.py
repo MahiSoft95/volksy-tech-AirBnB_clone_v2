6f65340d08a1f360de6f5d89f03bc20df577a859
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-08 19:09:26 -0800

    python exceptions

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -3,6 +3,6 @@ def safe_function(fct, *args):
     import sys
     try:
         return(fct(*args))
-    except(TypeError, ValueError)as error:
+    except Exception as error:
         sys.stderr.write("Exception: {}\n".format(error))
         return False
