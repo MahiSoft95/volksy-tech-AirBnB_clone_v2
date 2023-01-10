49927921716b5ba4bed6ecba6c5a1da94d1bbda6
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 14:43:10 -0800

    helloo

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,6 @@ class Square:
 
 
     def __init__(self, size=0):
-        """Hellooo Initialization how are you"""
         if type(size) not int:
             raise TypeError("size must be an integer")
         if size < 0:
