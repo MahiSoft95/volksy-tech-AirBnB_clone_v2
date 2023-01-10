8d908773200cb4441e634050fde7e92e6752b41a
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 14:37:41 -0800

    helloo

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,8 +7,7 @@ class Square:
     An empty class and
     this is my first class"""
     def __init__(self, size=0):
-        """TypeError and ValueError raised in this initialization"""
-        if size not int:
+        if type(size) not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
