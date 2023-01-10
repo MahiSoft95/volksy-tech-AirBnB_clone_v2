3a3a4f88f9a90412d41b4be2644a6324b36773cd
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 14:13:36 -0800

    helloo

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square class started from here"""
+
+
+class Square:
+    """This class creates
+    An empty class and
+    this is my first class"""
+    def demo(self, size):
+        self.__size = size
