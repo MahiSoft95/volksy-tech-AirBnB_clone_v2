627862d8f9c08657d3cf229598470341ad472a61
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 14:56:35 -0800

    helloo

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     this is my first class"""
     def __init__(self, size=0):
         """Square"""
-        if type(size) not int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
