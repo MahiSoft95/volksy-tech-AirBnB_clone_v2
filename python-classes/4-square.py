fe9b58df6a0acdea53d2a180f0a7923ce669fb11
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 15:22:36 -0800

    helloo

---
+++ python-classes/4-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3 here
+"""creates class Square with private instance attribute size"""
+
+
+class Square:
+    """The square class"""
+    def __init__(self, size=0):
+        self.__size = size
+    def size(self, value):
+        """Square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
