73ebd1f3dd20002a4cd76b55440c945581e97178
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 15:48:31 -0800

    helloo

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,8 +6,8 @@ class Square:
     """The square class"""
     def __init__(self, size=0):
         self.__size = size
+
     def size(self, value):
-        """Square"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
