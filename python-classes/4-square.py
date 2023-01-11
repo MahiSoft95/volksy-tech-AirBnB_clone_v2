aeba9380a86c50d0660341a8fef9881c6fcfcb4b
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 16:08:39 -0800

    helloo

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -15,7 +15,7 @@ class Square:
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
