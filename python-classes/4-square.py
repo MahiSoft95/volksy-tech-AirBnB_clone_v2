5afd7c596f394f2c3b25ca7256f534f25a4176e6
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 16:10:20 -0800

    helloo

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,12 +6,10 @@ class Square:
     """The square class"""
     def __init__(self, size=0):
         self.__size = size
-
-    @property
+    
     def size(self):
         return(self.__size)
 
-    @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
