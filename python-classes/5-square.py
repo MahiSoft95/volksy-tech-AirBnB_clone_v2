fe8e501565b8a5447187c5c58c2083c9307f3862
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-10 18:19:53 -0800

    helloo

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -7,11 +7,6 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
     
-    @property    
-    def size(self):
-        return(self.__size)
-
-    @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -22,3 +17,11 @@ class Square:
     def area(self):
         """""Hi area"""
         return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size > 0:
+            for column in range(self.__size):
+                for row in range(self.__size):
+                    print("#", end="")
+        else:
+            print()
