073c5ce3c291921500a4e0235b38e05caa4f1eaa
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-13 01:04:52 -0800

    hellooo

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Mahesh"""
+
+Rectangle = __import__('8i-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Mahesh"""
+    def __init__(self, size):
+        """Mahesh"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Mahesh"""
+        return (self.__size * self.__size)
+    
+    def __str__(self):
+        """Mahesh"""
+        string = "[Square] " + str(self.__size) + "/" + str(self.__size)
+        return string
