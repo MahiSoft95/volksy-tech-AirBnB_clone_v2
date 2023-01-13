b8039fd07705a5712772ca532e0b56698198f207
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-13 02:13:55 -0800

    hellooo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     def area(self):
         """Not implemented."""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Mahesh"""
         if type(value) is not int:
