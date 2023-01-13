ab71028cb5d53d75d122cfe06038c4815e3a5f46
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-13 00:46:04 -0800

    hellooo

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-    
+
     def area(self):
         """Mahesh"""
         return (self.__size * self.__size)
