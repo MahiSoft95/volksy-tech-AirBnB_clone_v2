35c65339b72ccbefacee4ce3648a844f4c5517f4
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-18 19:42:18 -0800

    helloo

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Using with statement writing into the file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """def write_file(filename="", text=""):"""
     with open(filename, 'a') as f:
         return f.write(text)
