4df935deeae988723e567e98b8a4bac13ea11437
MahiSoft95 <maheshs@brightcomgroup.com>
2023-01-18 19:19:29 -0800

    helloo

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Using with statement read and print data in stdout"""
+
 
 def read_file(filename=""):
     """def read_file(filename=""):"""
-    with open(filename, 'r')as f:
-        print(f.read())
+    with open(filename, 'r') as f:
+        print(f.read(), end="")
