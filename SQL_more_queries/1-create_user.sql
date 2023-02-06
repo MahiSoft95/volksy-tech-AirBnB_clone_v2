62cae734c01228b20b8755be66cbb7c49598fb7e
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-06 10:35:56 -0800

    sql more

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates MySQL server user user_0d_1 with all previlages
 -- user_0d_1 should have password user_0d_1_pwd
 CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost;
