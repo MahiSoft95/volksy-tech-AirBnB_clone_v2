9a6609004b37b11428f20299390ed7df182d38fc
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-06 10:56:44 -0800

    sql more

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- create table
 -- id default 1 must be UNIQUE name
-CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1 UNIQUE, name VARCHAR(256)NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256)NOT NULL);
