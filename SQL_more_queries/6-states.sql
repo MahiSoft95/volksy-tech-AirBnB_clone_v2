9f5b1fd5184ff315cb9556fe33fd35563c54409a
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-06 11:08:39 -0800

    sql more

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- create db if not exists
+-- create table... id int autoincrement primarykey
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
