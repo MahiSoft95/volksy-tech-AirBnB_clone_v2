a362ddaef9a0492137c5bac7f6db0b03deab73bc
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-06 10:12:21 -0800

    sql more

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of MySQL users user1 and user2
 -- previleges are listed on localhost server
-SHOW GRANTS FOR user_0d@localhost;
-SHOW GRANTS FOR user_od_2@localhost;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
