6f697f15be3dd9587ff1a195277c1e30660e785b
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-06 11:57:08 -0800

    sql more

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- inner join
-SELECT c.`id`,`c.name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON c.`state_id` = s.`id` ORDER BY c.`id` ASC;
+SELECT c.`id`, c.`name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON c.`state_id` = s.`id` ORDER BY c.`id` ASC;
