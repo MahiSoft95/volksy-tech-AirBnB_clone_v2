99fbe7f63285618eb5dd6cd3ad903421c5e2abb1
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-06 11:32:22 -0800

    sql more

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@
 -- state_id INT cant be null must be a foreign key that references to id of the states table
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_od_usa.cities(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 id INT AUTO_INCREMENT PRIMARY KEY,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
