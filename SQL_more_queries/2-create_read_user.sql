35b99901fddf7f282c4dbade574fc4d7e9dd9cfc
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-06 10:45:10 -0800

    sql more

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- create db if not exists
+-- create user if not exists with password
+-- grant only select On db.* To user@loclahost
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
