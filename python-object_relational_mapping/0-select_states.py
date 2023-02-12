bdd46939b7a138ecef90a55390566b58f51cfe5b
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-12 10:39:46 -0800

    Maheshh is king forever!

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,7 @@
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    """hellooo"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
