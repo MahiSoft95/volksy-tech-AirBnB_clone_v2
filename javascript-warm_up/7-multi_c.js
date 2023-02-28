b522062d3fdf8c5ac6e4bb863e58758702fd883b
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-28 04:44:32 -0800

    Mahesh is king forever DA!

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const text = 'C is fun';
-num = Number(process.argv.slice(2));
+const num = Number(process.argv.slice(2));
 if (isNaN(num)) {
-	console.log('Missing number of occurrences');
-  } else {
-	let i = 0;
-	for (let i = 0; i < num;  i++) {
+  console.log('Missing number of occurrences');
+else {
+ let i = 0;
+for (let i = 0; i < num;  i++) {
 		console.log(text);
-	}
+}
   }
