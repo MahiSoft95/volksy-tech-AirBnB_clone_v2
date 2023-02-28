d5958d8ee1769b56340050ab5b8351ca148a5176
MahiSoft95 <maheshs@brightcomgroup.com>
2023-02-27 22:55:46 -0800

    Mahesh is king forever DA!

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cm = process.argv
+if (cm.length<3){
+  console.log('No argument')}
+else if (cm.length == 3){
+  console.log('Argument found')}
+else{
+  console.log('Arguments found')}
